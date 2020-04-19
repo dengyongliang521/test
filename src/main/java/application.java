fe9ec22be3fb96f86ac4f68ab3039cad2ff23eb2
dengyongliang <dengyongliang@163.com>
2020-04-19 16:09:38 +0800

    第一次教

---
+++ src/main/java/application.java
@@ -0,0 +1,15 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: dengYongLiang
+ * @Date: 2020/04/19 14:31
+ * @Description:
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class application {
+    public static void main(String[] args) {
+        SpringApplication.run(application.class, args);
+    }
+}
---
+++ src/main/java/test.java
@@ -0,0 +1,8 @@
+/**
+ * @Auther: dengYongLiang
+ * @Date: 2020/04/19 14:24
+ * @Description:
+ * @Version 1.0
+ */
+public class test {
+}
